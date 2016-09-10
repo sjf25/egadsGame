9e2315388b33012a621b72e17bdc3088a084aacb
kevinlam508 <kevin.lam72003@gmail.com>
2016-09-10 16:53:09 -0500

    scripts for death

---
+++ Assets/DamageOnContact.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnContact : MonoBehaviour {
+
+    public int damage;
+    public bool touching;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        touching = true;
+        GameObject obj = coll.gameObject;
+        if(obj.tag == "Player 1" | obj.tag == "Player 2")
+            coll.gameObject.GetComponent<Character>().health -= damage; 
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        touching = false;
+    }
+}
--- Assets/Spawnpoint.cs
+++ Assets/Spawnpoint.cs
@@ -4,18 +4,19 @@ using System.Collections;
 public class Spawnpoint : MonoBehaviour {
 
     public GameObject player;
-    
+    private Character script;
 
 	// Use this for initialization
 	void Start () {
-	
+        script = player.GetComponent<Character>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if(player.GetComponent("Character").isDead())
+        if (script.isDead())
         {
-            player.transform.position = this.transform.position;
+            player.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+            script.health = script.maxHealth;
         }
 	}
 }
