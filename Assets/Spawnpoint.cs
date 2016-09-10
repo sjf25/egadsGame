a62495979bb1b320b9ce4ffd5ebed7b48ad4a37b
Kevin Lam <kevin.lam72003@gmail.com>
2016-09-10 14:42:52 -0500

    respawn test

---
+++ Assets/Spawnpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawnpoint : MonoBehaviour {
+
+    public GameObject player;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(player.GetComponent("Character").isDead())
+        {
+            player.transform.position = this.transform.position;
+        }
+	}
+}
