0ebae6752ad5ea1842b1c0261a3527a3b7c759cb
kevinlam508 <kevin.lam72003@gmail.com>
2016-09-10 22:11:39 -0500

    upgraded shooting

---
+++ Assets/Assets/Projectile.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float velocity = 1F;
+    public int damage = 1;
+    public int health = 1;
+    public bool music = false;
+    public int deleteDistance = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(this.transform.position.x + velocity * Time.deltaTime, this.transform.position.y);
+        if(this.health <= 0 | Mathf.Abs(this.gameObject.transform.position.x) > deleteDistance)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Ground")
+            Destroy(this.gameObject);
+        if (this.tag == "Bullet 1")
+        {
+            if (!music & coll.gameObject.tag == "Bullet 2")
+                coll.gameObject.GetComponent<Projectile>().health -= damage;
+            else if (coll.gameObject.tag == "Player 2")
+            {
+                coll.gameObject.GetComponent<Character>().health -= damage;
+                health -= 1;
+            }
+        }
+        else if (this.tag == "Bullet 2")
+        {
+            if (!music & coll.gameObject.tag == "Bullet 1")
+                coll.gameObject.GetComponent<Projectile>().health -= damage;
+            else if (coll.gameObject.tag == "Player 1")
+            {
+                coll.gameObject.GetComponent<Character>().health -= damage;
+                health -= 1;
+            }
+        }
+    }
+
+    public void CorrectImage()
+    {
+        this.transform.localScale *= velocity / Mathf.Abs(velocity);
+    }
+}
---
+++ Assets/Assets/Shooting.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+    public GameObject projectile;
+    public GameObject music;
+    public GameObject player;
+    Character scriptPlayer;
+    private float cooldown;
+    public float fireDelay = .5F;
+    public bool shooting = false;
+    public int offsetY;
+
+	// Use this for initialization
+	void Start () {
+        scriptPlayer = player.GetComponent<Character>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cooldown -= Time.deltaTime;
+        if (Input.GetKeyDown(scriptPlayer.shoot))
+            shooting = true;
+        if (Input.GetKeyUp(scriptPlayer.shoot))
+            shooting = false;
+
+        if(shooting & cooldown <= 0)
+        {
+            cooldown = fireDelay;
+            GameObject bullet = Instantiate(projectile);
+            bullet.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offsetY);
+            switch (player.tag)
+            {
+                case "Player 1":
+                    bullet.tag = "Bullet 1";
+                    break;
+                case "Player 2":
+                    bullet.tag = "Bullet 2";
+                    break;
+            }
+            if (!scriptPlayer.facingRight)
+                bullet.GetComponent<Projectile>().velocity = -(bullet.GetComponent<Projectile>().velocity);
+            bullet.GetComponent<Projectile>().CorrectImage();
+        }
+
+	}
+}
