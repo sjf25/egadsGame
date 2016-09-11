6ed8dcff251906fcd2e3673e02aec3a53e846c22
sjf25 <sohaibjf@gmail.com>
2016-09-10 20:36:21 -0500

    Add files via upload

---
+++ Assets/Assets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	private GameObject person1, person2;
+	private int padding = 5;
+	// Use this for initialization
+	void Start () {
+		person1 = GameObject.FindWithTag ("Person 1");
+		person2 = GameObject.FindWithTag ("Person 2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera mainCam = Camera.main;
+		GameObject camGame = Camera.main.gameObject;
+		Vector3 p1Pos = person1.transform.position;
+		Vector3 p2Pos = person2.transform.position;
+		if (Mathf.Abs (p1Pos.x - p2Pos.x) > Mathf.Abs (p1Pos.y - p2Pos.y)) {
+			mainCam.orthographicSize = (Mathf.Abs (p1Pos.x - p2Pos.x) + padding) / (2 * mainCam.aspect);
+			//camGame.
+		}
+	}
+}
