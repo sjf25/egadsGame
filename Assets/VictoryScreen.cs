e684bc1fefa9c557703d40e95f1a50fa05f27395
kevinlam508 <kevin.lam72003@gmail.com>
2016-09-11 09:11:30 -0500

    victory screen script

---
+++ Assets/VictoryScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryScreen : MonoBehaviour {
+
+    public float countdown = 6F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("arena");
+        }
+	}
+}
